823910555ec53a5854612788982e9e3d9bd2bd2d
dennispaul8 <jusmepaul8@gmail.com>
2025-10-08 04:28:10 +0100

    fix apibase url

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
   })
 );
