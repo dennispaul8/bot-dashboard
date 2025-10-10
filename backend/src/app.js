6e0b91e361f060430a700d5d3b971b1fc0db495a
dennispaul8 <jusmepaul8@gmail.com>
2025-10-10 09:19:14 +0100

    edit frontend url for cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 // ✅ CORS: allow cookies (for Twitter OAuth redirect)
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
