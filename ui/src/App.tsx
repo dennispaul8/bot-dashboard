d9eef869c85e8b9d5574912d1f27973e865a4233
dennispaul8 <jusmepaul8@gmail.com>
2025-10-14 16:09:19 +0100

    fix milestone tooltip

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -588,7 +588,7 @@ export default function App() {
                                 ? "#1e293b"
                                 : "#0f172a",
                             border: "none",
-                            color: theme === "default" ? "#fff" : "#fff",
+                            color: theme === "default" ? "#000" : "#fff",
                           }}
                         />
                         <Bar dataKey="count" radius={[8, 8, 0, 0]}>
